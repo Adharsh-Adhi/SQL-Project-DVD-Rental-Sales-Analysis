5724fa2a342194ffc96d9b7fd8a3601a17d5ebe0
Adharsh-Adhi <adharshadhi2003@gmail.com>
2025-11-30 21:23:00 +0530

    Add files via upload
    Initial upload of SQL project files

---
+++ 02_films_with_deleted_scenes.sql
@@ -0,0 +1,28 @@
+title,special_features,release_year
+"ACADEMY DINOSAUR","Deleted Scenes,Behind the Scenes",2006
+"ACE GOLDFINGER","Trailers,Deleted Scenes",2006
+"ADAPTATION HOLES","Trailers,Deleted Scenes",2006
+"AFRICAN EGG","Deleted Scenes",2006
+"AGENT TRUMAN","Deleted Scenes",2006
+"AIRPLANE SIERRA","Trailers,Deleted Scenes",2006
+"ALABAMA DEVIL","Trailers,Deleted Scenes",2006
+"ALADDIN CALENDAR","Trailers,Deleted Scenes",2006
+"ALASKA PHANTOM","Commentaries,Deleted Scenes",2006
+"ALI FOREVER","Deleted Scenes,Behind the Scenes",2006
+"ALICE FANTASIA","Trailers,Deleted Scenes,Behind the Scenes",2006
+"AMADEUS HOLY","Commentaries,Deleted Scenes,Behind the Scenes",2006
+"AMELIE HELLFIGHTERS","Commentaries,Deleted Scenes,Behind the Scenes",2006
+"ANACONDA CONFESSIONS","Trailers,Deleted Scenes",2006
+"ANNIE IDENTITY","Commentaries,Deleted Scenes",2006
+"ANONYMOUS HUMAN","Deleted Scenes,Behind the Scenes",2006
+"ANTHEM LUKE","Deleted Scenes,Behind the Scenes",2006
+"ANTITRUST TOMATOES","Trailers,Commentaries,Deleted Scenes",2006
+"ANYTHING SAVANNAH","Trailers,Deleted Scenes,Behind the Scenes",2006
+"APACHE DIVINE","Commentaries,Deleted Scenes,Behind the Scenes",2006
+"APOLLO TEEN","Trailers,Commentaries,Deleted Scenes,Behind the Scenes",2006
+"ARABIA DOGMA","Commentaries,Deleted Scenes",2006
+"ARACHNOPHOBIA ROLLERCOASTER","Trailers,Deleted Scenes,Behind the Scenes",2006
+"ARIZONA BANG","Trailers,Deleted Scenes",2006
+"ARK RIDGEMONT","Trailers,Commentaries,Deleted Scenes,Behind the Scenes",2006
+"ARSENIC INDEPENDENCE","Trailers,Deleted Scenes,Behind the Scenes",2006
+"AUTUMN CROW","Trailers,Commentaries,Deleted Scenes,Behind the Scenes",2006
---
+++ Readme.txt
@@ -0,0 +1,52 @@
+
+
+---
+
+# **Sakila SQL Analysis Project**
+
+This project analyzes the Sakila DVD Rental Database using SQL.
+The objective is to explore customer behavior, film performance, and rental trends by writing SQL queries and exporting the results for further analysis.
+
+## **Project Files**
+
+* 01_pg13_low_rental_rate_films.csv
+* 02_films_with_deleted_scenes.csv
+* 03_active_customers_list.csv
+* 04_customers_who_rented_on_date.csv
+* 05_distinct_customers_on_date.csv
+* 06_daily_rental_counts.csv
+* 07_scifi_films_list.csv
+* 08_customer_rental_counts.csv
+* 09_films_to_discontinue_low_rentals.csv
+* 10_films_not_returned.csv
+
+## **Tools Used**
+
+* MySQL
+* MySQL Workbench
+* CSV
+* Excel
+
+## **Skills Demonstrated**
+
+* SQL SELECT, WHERE, ORDER BY
+* JOIN operations
+* GROUP BY and HAVING
+* Aggregate functions (COUNT, SUM)
+* Exporting data from SQL
+* Data exploration and reporting
+
+## **Key Insights**
+
+* Identified films with low rental frequency.
+* Analyzed customer rental activity on specific dates.
+* Counted daily rentals.
+* Identified active customers and key film categories.
+
+## **Conclusion**
+
+This project strengthened my SQL fundamentals and helped me understand how SQL is used in real business data scenarios.
+
+---
+
+
